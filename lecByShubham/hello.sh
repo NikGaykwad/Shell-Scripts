e3f2c6d6352f0020dab85ba97b8920da8360379b
Nik Gaykwad <nikhilgaykwad176@gmail.com>
2025-04-07 11:28:07 +0000

    Added new shell scripts in Shell Scripting folder

---
+++ lecByShubham/hello.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+
+#This is called Shebang
+echo "Hello, world!"
+echo "This is new shell scripting file with TWS"
+echo "Welcome in Mumbai"
+echo "Make some noise guys"
+
+
+: '
+This is a multi-line comment
+It won’t be executed
+'
---
+++ lecByShubham/jethalal_ki_duniya.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a normal file for variables
+
+name="Jethalal"  # No space before '='
+
+echo "Name is $name and date is $(date)"
+
+echo "Kindly enter your name: "
+
+read username
+
+echo "You entered $username"
+
